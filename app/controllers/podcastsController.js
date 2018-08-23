16b031a0085bfad980cf39b7af753b401591809a
Ashley Thompson <ashley.thompson769@gmail.com>
2018-08-23 17:40:56 -0400

    Added eslint

--- app/controllers/podcastsController.js
+++ app/controllers/podcastsController.js
@@ -1,70 +1,69 @@
-const db =  require('../models/podcasts');
+const db = require('../models/podcasts');
 
 function showAll(req, res) {
-    db.allPodcasts()
-    .then(podcasts => {
-        res.json(podcasts);
+  db.allPodcasts()
+    .then((podcasts) => {
+      res.json(podcasts);
     })
-    .catch(err => {
-        console.log(err);
-        res.status(400).json({message: '400', err});
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json({ message: '400', err });
     });
-};
+}
 
 function showOne(req, res) {
-    db.onePodcast(req.params.id)
-    .then(podcast => {
-        res.json(podcast);
+  db.onePodcast(req.params.id)
+    .then((podcast) => {
+      res.json(podcast);
     })
-    .catch(err => {
-        console.log(err);
-        res.status(400).json({message: '400', err});
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json({ message: '400', err });
     });
-};
+}
 
 function createPodcast(req, res) {
-    db.createPodcast(req.body)
-    .then(podcast => {
-        res.json({
-            message: 'ok',
-            data: { podcast },
-        });
+  db.createPodcast(req.body)
+    .then((podcast) => {
+      res.json({
+        message: 'ok',
+        data: { podcast },
+      });
     })
-    .catch(err => {
-        console.log(err);
-        res.status(400).json({message: '400', err});
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json({ message: '400', err });
     });
-};
+}
 
 function updatePodcast(req, res) {
-    db.updatePodcast(req.body, req.params.id)
-    .then(podcast => {
-        res.json({
-            message: 'ok',
-            data: { podcast },
-        });
+  db.updatePodcast(req.body, req.params.id)
+    .then((podcast) => {
+      res.json({
+        message: 'ok',
+        data: { podcast },
+      });
     })
-    .catch(err => {
-        console.log(err);
-        res.status(400).json({message: '400', err});
+    .catch((err) => {
+      console.log(err);
+      res.status(400).json({ message: '400', err });
     });
 }
 
 function deletePodcast(req, res) {
-    db.deletePodcast(req.params.id)
-    .then(podcast => {
-        res.json({
-            message: 'ok',
-        });
-    })
+  db.deletePodcast(req.params.id)
+    .then((podcast) => {
+      res.json({
+        message: 'ok',
+      });
+    });
 }
-
 
 
 module.exports = {
-    showAll,
-    showOne,
-    createPodcast,
-    updatePodcast,
-    deletePodcast
-}
+  showAll,
+  showOne,
+  createPodcast,
+  updatePodcast,
+  deletePodcast,
+};
