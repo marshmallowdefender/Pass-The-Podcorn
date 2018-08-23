51122148fe05f3ad2d7f50a9f8883c8be745b2e1
Ashley Thompson <ashley.thompson769@gmail.com>
2018-08-23 15:17:21 -0400

    Deleted excess data in sortbuttons

--- client/src/components/SortButtons.jsx
+++ client/src/components/SortButtons.jsx
@@ -2,13 +2,8 @@ import React from 'react';
 
 function SortButtons(props) {
     return (
-<<<<<<< HEAD
-        <div>
-            <input onChange={props.search} value={props.searchBar} name="text-name" type="text" placeholder="Search"/>
-=======
         <div className="sortBtnDiv">
-            <input onChange={props.search} value={props.searchBar} name="text-name" type="text" placeholder="Search Genre"/>
->>>>>>> master
+            <input onChange={props.search} value={props.searchBar} name="text-name" type="text" placeholder="Search"/>
             <button onClick={e => props.filterFunction('All')} type="button" className="btn podcastSortBtn">All</button>
             <button onClick={e => props.filterFunction('Gaming')} type="button" className="btn podcastSortBtn">Gaming</button>
             <button onClick={e => props.filterFunction('True Crime')} type="button" className="btn podcastSortBtn">True Crime</button>
