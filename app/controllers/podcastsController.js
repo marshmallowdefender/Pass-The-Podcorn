8d5222fb9470bcf16f8a2b9eacf29f370fd1e5f5
Ashley Thompson <ashley.thompson769@gmail.com>
2018-08-23 18:06:42 -0400

    Edited podcastController

--- app/controllers/podcastsController.js
+++ app/controllers/podcastsController.js
@@ -2,17 +2,10 @@ const db = require('../models/podcasts');
 
 function showAll(req, res) {
   db.allPodcasts()
-<<<<<<< HEAD
-    .then((podcasts) => {
-      res.json(podcasts);
-    })
-    .catch((err) => {
-=======
     .then(podcasts => {
       res.json(podcasts);
     })
     .catch(err => {
->>>>>>> master
       console.log(err);
       res.status(400).json({ message: '400', err });
     });
@@ -20,17 +13,10 @@ function showAll(req, res) {
 
 function showOne(req, res) {
   db.onePodcast(req.params.id)
-<<<<<<< HEAD
-    .then((podcast) => {
-      res.json(podcast);
-    })
-    .catch((err) => {
-=======
     .then(podcast => {
       res.json(podcast);
     })
     .catch(err => {
->>>>>>> master
       console.log(err);
       res.status(400).json({ message: '400', err });
     });
@@ -38,21 +24,13 @@ function showOne(req, res) {
 
 function createPodcast(req, res) {
   db.createPodcast(req.body)
-<<<<<<< HEAD
-    .then((podcast) => {
-=======
     .then(podcast => {
->>>>>>> master
       res.json({
         message: 'ok',
         data: { podcast },
       });
     })
-<<<<<<< HEAD
-    .catch((err) => {
-=======
     .catch(err => {
->>>>>>> master
       console.log(err);
       res.status(400).json({ message: '400', err });
     });
@@ -60,21 +38,11 @@ function createPodcast(req, res) {
 
 function updatePodcast(req, res) {
   db.updatePodcast(req.body, req.params.id)
-<<<<<<< HEAD
-    .then((podcast) => {
-      res.json({
-        message: 'ok',
-        data: { podcast },
-      });
-    })
-    .catch((err) => {
-=======
     .then(podcast => {
       res.json(podcast)
     }
     )
     .catch(err => {
->>>>>>> master
       console.log(err);
       res.status(400).json({ message: '400', err });
     });
@@ -82,19 +50,11 @@ function updatePodcast(req, res) {
 
 function deletePodcast(req, res) {
   db.deletePodcast(req.params.id)
-<<<<<<< HEAD
-    .then((podcast) => {
-      res.json({
-        message: 'ok',
-      });
-    });
-=======
     .then(podcast => {
       res.json({
         message: 'ok',
       });
     })
->>>>>>> master
 }
 
 
@@ -103,10 +63,5 @@ module.exports = {
   showOne,
   createPodcast,
   updatePodcast,
-<<<<<<< HEAD
-  deletePodcast,
-};
-=======
   deletePodcast
 }
->>>>>>> master
